bf778e83ea06a093141778d1188195827ad1cc78
Jacob Yoon <jacobyoon926@gmail.com>
2017-12-31 12:36:18 -0500

    Test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+jQuery(document).ready(function($) {  
+
+// site preloader -- also uncomment the div in the header and the css style for #preloader
+$(window).load(function(){
+  $('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+});
+
 (function() {
   var MutationObserver, Util, WeakMap, getComputedStyle, getComputedStyleRX,
     bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
