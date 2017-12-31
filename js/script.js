619d7c543818ff4376dfbfef3eb48159f47b587d
Jacob Yoon <jacobyoon926@gmail.com>
2017-12-31 12:44:03 -0500

    Loading test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// click "Run" to replay
+
+var $body = $('body');
+
+var loading = [
+    { elements: $body, properties: { width: '20%' } },
+    { elements: $body, properties: { width: '30%' } },
+    { elements: $body, properties: { width: '50%' } },
+    { elements: $body, properties: { width: '100%' } },
+    { elements: $body, properties: { height: '100%' }, options: { 
+      complete: function () { 
+        $('.wrap').velocity( 'transition.slideUpIn' );
+        $('img').velocity( 'transition.flipYIn' );
+        $('html').css({ background: '#fff' });
+      }
+    }
+  }
+]; 
+
+$.Velocity.RunSequence(loading);
+
 (function() {
   var MutationObserver, Util, WeakMap, getComputedStyle, getComputedStyleRX,
     bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
